ddcc955c946eec546fd81a620a3e8cae4cf56adb
ruben <ruben_m7@outlook.es>
2019-12-14 00:37:18 +0100

    zedisplaycore base

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,7 +3,7 @@ $(function() {
     var passwordEl = $('#password');
 
     // in a real application, hardcoding the user/password would be idiotic
-    // but for the demo application it's very convenient to do so
+    // but for the zedisplaycore application it's very convenient to do so
     if (!usernameEl.val() || 'jane_admin' === usernameEl.val()) {
         usernameEl.val('jane_admin');
         passwordEl.val('kitten');
