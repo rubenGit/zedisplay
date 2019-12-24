dbd4bd5ec4e177311f64bfbfd162758ef1622c76
ruben <ruben_m7@outlook.es>
2019-12-24 13:06:48 +0100

    add new table Channel

---
+++ src/Entity/Channel.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ChannelRepository")
+ */
+class Channel
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contentents;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContentents(): ?string
+    {
+        return $this->contentents;
+    }
+
+    public function setContentents(string $contentents): self
+    {
+        $this->contentents = $contentents;
+
+        return $this;
+    }
+}
