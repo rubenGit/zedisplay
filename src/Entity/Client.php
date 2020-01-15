74eac4b66ed098908b78eb09a6b92e943c1c0b79
ruben <ruben_m7@outlook.es>
2020-01-15 12:22:05 +0100

    add inverse relation with client

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -89,6 +89,11 @@ class Client
      *@ORM\OneToMany(targetEntity="App\Entity\Content", mappedBy="client")
      */
     private $contents;
+    /**
+     *@ORM\OneToMany(targetEntity="App\Entity\Channel", mappedBy="client")
+     */
+    private $channels;
+
 
     public function __construct()
     {
@@ -101,6 +106,7 @@ class Client
         $this->establishments = new ArrayCollection();
         $this->devices = new ArrayCollection();
         $this->contents = new ArrayCollection();
+        $this->channels = new ArrayCollection();
     }
 
     /**
@@ -377,6 +383,37 @@ class Client
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Channel[]
+     */
+    public function getChannels(): Collection
+    {
+        return $this->channels;
+    }
+
+    public function addChannel(Channel $channel): self
+    {
+        if (!$this->channels->contains($channel)) {
+            $this->channels[] = $channel;
+            $channel->setClient($this);
+        }
+
+        return $this;
+    }
+
+    public function removeChannel(Channel $channel): self
+    {
+        if ($this->channels->contains($channel)) {
+            $this->channels->removeElement($channel);
+            // set the owning side to null (unless already changed)
+            if ($channel->getClient() === $this) {
+                $channel->setClient(null);
+            }
+        }
 
         return $this;
     }
